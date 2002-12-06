2dec2e06c5256b69f0261c525788ec4ad77654a1
aarvesen <aarvesen>
2002-12-06 21:38:33 +0000

    New factory registration in the constructor

--- com/p6spy/engine/sample/P6SampleConnection.java
+++ com/p6spy/engine/sample/P6SampleConnection.java
@@ -59,60 +59,6 @@
  * SUCH DAMAGE.
  */
 
-/**
- * Description: Wrapper class for Connection
- *
- * $Author$
- * $Revision$
- * $Date$
- *
- * $Id$
- * $Log$
- * Revision 1.1  2002/11/26 18:17:26  jeffgoke
- * *** empty log message ***
- *
- * Revision 1.1  2002/10/06 18:24:52  jeffgoke
- * no message
- *
- * Revision 1.1  2002/05/24 07:31:45  jeffgoke
- * version 1 rewrite
- *
- * Revision 1.7  2002/05/18 06:39:52  jeffgoke
- * Peter Laird added Outage detection.  Added junit tests for outage detection.
- * Fixed multi-driver tests.
- *
- * Revision 1.6  2002/05/16 04:58:40  jeffgoke
- * Viktor Szathmary added multi-driver support.
- * Rewrote P6SpyOptions to be easier to manage.
- * Fixed several bugs.
- *
- * Revision 1.5  2002/04/27 20:24:01  jeffgoke
- * added logging of commit statements and rollback statements
- *
- * Revision 1.4  2002/04/11 04:18:03  jeffgoke
- * fixed bug where callable & prepared were not passing their ancestors the correct constructor information
- *
- * Revision 1.3  2002/04/10 04:24:26  jeffgoke
- * added support for callable statements and fixed numerous bugs that allowed the real class to be returned
- *
- * Revision 1.2  2002/04/07 20:43:59  jeffgoke
- * fixed bug that caused null connection to return an empty connection instead of null.
- * added an option allowing the user to truncate.
- * added a release target to the build to create the release files.
- *
- * Revision 1.1.1.1  2002/04/07 04:52:25  jeffgoke
- * no message
- *
- * Revision 1.2  2001-08-02 07:52:43-05  andy
- * <>
- *
- * Revision 1.1  2001-07-30 23:03:31-05  andy
- * <>
- *
- * Revision 1.0  2001-07-30 17:46:22-05  andy
- * Initial revision
- *
- */
 
 package com.p6spy.engine.sample;
 
@@ -122,12 +68,14 @@ import java.sql.*;
 import java.util.*;
 
 public class P6SampleConnection extends P6Connection implements java.sql.Connection {
+    protected int count = 0;    
     
-    protected P6Factory getP6Factory() {
-        return new P6SampleFactory();
+    public P6SampleConnection(P6Factory factory, Connection conn) throws SQLException {
+        super(factory, conn);
     }
     
-    public P6SampleConnection(Connection conn) throws SQLException {
-        super(conn);
+    public void commit() throws SQLException {
+	super.commit();
+	System.out.println("This connection has committed " + (++count) + " times");
     }
 }
