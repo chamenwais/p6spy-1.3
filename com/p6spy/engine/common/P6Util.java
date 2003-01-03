088b2408bcdd056cfe1dca53eb92b70fd3434f3a
aarvesen <aarvesen>
2003-01-03 21:14:54 +0000

    added the (unused) removeDots method added the (widely used) forName method to implement better class loading

--- com/p6spy/engine/common/P6Util.java
+++ com/p6spy/engine/common/P6Util.java
@@ -69,6 +69,10 @@
  *
  * $Id$
  * $Log$
+ * Revision 1.3  2003/01/03 21:14:54  aarvesen
+ * added the (unused) removeDots method
+ * added the (widely used) forName method to implement better class loading
+ *
  * Revision 1.2  2002/10/06 18:21:37  jeffgoke
  * no message
  *
@@ -193,6 +197,7 @@ public class P6Util {
             } else {
                 FileInputStream in = new FileInputStream(path);
                 props.load(in);
+		//removeDots(props);
                 in.close();
             }
         } catch (FileNotFoundException e1) {
@@ -203,6 +208,35 @@ public class P6Util {
         
         return props;
     }
+
+    protected static void removeDots(Properties props) {
+	Enumeration keys = props.keys();
+	HashMap hash     = new HashMap();
+	boolean done     = false;
+
+	for (; keys.hasMoreElements();) {
+	    String key = (String) keys.nextElement();
+	    if (key.indexOf('.') != -1) {
+		int len    = key.length();
+		int newLen = 0;
+		char[] car = new char[len];
+		for (int i = 0; i < len; i++) {
+		    char c = key.charAt(i);
+		    if (c != '.') {
+			car[newLen++] = c;
+		    }
+		}
+
+		String out = new String(car, 0, newLen);
+		hash.put(out, props.getProperty(key));
+	    }
+	}
+
+	if (done) {
+	    props.putAll(hash);
+	}
+    }
+
     
     // this is our own version, which we need to do to ensure the order is kept
     // in the property file
@@ -344,4 +378,31 @@ public class P6Util {
         Method m = klass.getDeclaredMethod(method, null);
         return (m.invoke(null,null));
     }
+
+
+    /**
+     * A utiltity for using the current class loader (rather than the
+     * system class loader) when instantiating a new class.
+     * <p>
+     * The idea is that the thread's current loader might have an
+     * obscure notion of what your class path is (e.g. an app server) that
+     * will not be captured properly by the system class loader.
+     * <p>
+     * taken from http://sourceforge.net/forum/message.php?msg_id=1720229
+     *
+     * @param name class name to load
+     * @return the newly loaded class
+     */
+    public static Class forName(String name) throws ClassNotFoundException { 
+	ClassLoader ctxLoader = null; 
+	try { 
+	    ctxLoader = Thread.currentThread().getContextClassLoader(); 
+	    return Class.forName(name, true, ctxLoader); 
+
+	} catch(ClassNotFoundException ex) { 
+	    if(ctxLoader == null) { throw ex; } 
+	} catch(SecurityException ex) { 
+	} 
+	return Class.forName(name); 
+    } 
 }
